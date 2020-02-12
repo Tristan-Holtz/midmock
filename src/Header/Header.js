5449a316b20af1095b94cace1f79c96b42c41490
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-12 10:22:09 -0700

    Create header inputs

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 
-const Header = () => {
-  return <h1>Header</h1>;
-};
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: Date.now(),
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <header>
+        <h1>Turing Cafe Reservations</h1>
+        <form>
+          <input
+            placeholder="Name..."
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+          ></input>
+          <input
+            placeholder="Date (MM/DD)"
+            type="text"
+            name="date"
+            onChange={this.handleChange}
+          ></input>
+          <input
+            placeholder="Time"
+            type="text"
+            name="time"
+            onChange={this.handleChange}
+          ></input>
+          <input
+            placeholder="Number of guests"
+            type="text"
+            name="number"
+            onChange={this.handleChange}
+          ></input>
+          <button>Make Reservation</button>
+        </form>
+      </header>
+    );
+  }
+}
 
 export default Header;
