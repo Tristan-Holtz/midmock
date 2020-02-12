baa395b44f0e3cf7a6cbce505279b0cc89df01c8
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-12 09:54:37 -0700

    Get reseravtions on page

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import './Reservation.css';
 
-const Reservation = () => {
-  return <h1>On the page</h1>;
+const Reservation = ({ reservationList }) => {
+  const allReservations = reservationList.map(reservation => {
+    return (
+      <section className="reservation-card">
+        <h3>{reservation.name}</h3>
+        <p>{reservation.date}</p>
+        <p>{reservation.time}</p>
+        <p>{reservation.number}</p>
+        <button className="cancel-button">Cancel</button>
+      </section>
+    );
+  });
+  return <section className="reservation-section">{allReservations}</section>;
 };
 
 export default Reservation;
